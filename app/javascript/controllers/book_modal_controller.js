ceea9017a738beadf9bc1f57483d5c907e97a636
Hariram Yadav <hariramyadav12112@gmail.com>
2022-11-08 18:15:12 +0530

    basic design and make stimulus controller

---
+++ app/javascript/controllers/book_modal_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="book-modal"
+export default class extends Controller {
+  connect() {
+    alert("hello");
+  }
+  initialize() {
+    this.element.setAttribute("data-action", "click->book-modal#showModal")
+  }
+  
+  showModal(event) {
+    event.preventDefault()
+    this.url = this.element.getAttribute("href")
+     fetch(this.url,{
+      headers: {
+        Accept: "text/vnd.turbo-stream.html"
+      }
+    })
+    .then(response => response.text())
+    .then(html => Turbo.renderStreamMessage(html))
+  }
+}
